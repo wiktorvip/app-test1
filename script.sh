d4af44eae1bb5c09750d4566b3a1b37197005133
Wiktor <wiktor.vip@gmail.com>
2025-03-04 09:15:44 +0100

    update

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-watch -n 30 hexdump -C /dev/mem > /dev/null
-#watch -n 30 ls -la / > /dev/null
+#watch -n 30 hexdump -C /dev/mem > /dev/null
+watch -n 30 ls -la / > /dev/null
 #watch -n 30 ls -la /etc > /dev/null
